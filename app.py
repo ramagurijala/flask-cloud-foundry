8c85fe042c3a8885c588e9a69e940504c439b29c
unknown <grkrishna_mca@yahoo.com>
2019-05-25 23:12:23 +0100

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Python Flask running on Cloud Foundry!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
